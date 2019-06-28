daa0ad0c8ea0d041fa4f532830b60df8755250ee
Sean Alvis <me@seanalvis.com>
2019-06-27 20:47:04 -0400

    Using routes to set urls for the Part Info pages and passing the data to that page

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,7 +1,7 @@
 
 <template>
     <div class="part" :class="position">
-        <img :src="selectedPart.src" title="arm" />
+        <img @click="showPartInfo()" :src="selectedPart.src" title="arm" />
         <button @click="selectPreviousPart()" class="prev-selector"></button>
         <button @click="selectNextPart()" class="next-selector"></button>
         <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -66,6 +66,15 @@
                     this.parts.length,
                 );
             },
+            showPartInfo() {
+                this.$router.push({
+                    name: 'Parts',
+                    params: {
+                        id: this.selectedPart.id,
+                        partType: this.selectedPart.type,
+                    },
+                });
+            },
     
         },
     };
@@ -100,6 +109,7 @@
     
     .part img {
         width: 165px;
+        cursor: pointer;
     }
     
     .top {
